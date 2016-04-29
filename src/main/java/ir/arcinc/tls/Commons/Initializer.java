87a30f6fb86c3857431e7d0240e549ce7d17d2b2
Taha Eghtesad <tahaeghtesad@gmail.com>
2016-04-29 23:00:55 +0430

    Late receive feature added.

--- src/main/java/ir/arcinc/tls/Commons/Initializer.java
+++ src/main/java/ir/arcinc/tls/Commons/Initializer.java
@@ -13,9 +13,10 @@ public class Initializer {
     public Initializer(){
         double lossRate = 0.01;
         double errorRate = 0.001;
+        double lateRate = 0.05;
 
-        UnreliableChannel sendChannel = new UnreliableChannel(lossRate, errorRate);
-        UnreliableChannel receiveChannel = new UnreliableChannel(lossRate, errorRate);
+        UnreliableChannel sendChannel = new UnreliableChannel(lossRate, errorRate, lateRate);
+        UnreliableChannel receiveChannel = new UnreliableChannel(lossRate, errorRate, lateRate);
 
         Sender sender = new Sender(sendChannel);
         receiveChannel.addObserver(sender);
@@ -32,6 +33,7 @@ public class Initializer {
         senderApplication.createData();
         senderApplication.sendData();
 
+        System.out.println("Calculating jaccard coefficient...");
         double matchRate = receiverApplication.getJaccardCoefficient();
         System.out.println("Matched: " + String.format("%2.2f",matchRate*100) + "%");
     }
--- src/main/java/ir/arcinc/tls/Commons/UnreliableChannel.java
+++ src/main/java/ir/arcinc/tls/Commons/UnreliableChannel.java
@@ -1,7 +1,6 @@
 package ir.arcinc.tls.Commons;
 
-import java.util.Observable;
-import java.util.Random;
+import java.util.*;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
@@ -14,16 +13,32 @@ public class UnreliableChannel extends Observable{
 
     private double lossRate;
     private double errorRate;
+    private double lateRate;
 
-    public UnreliableChannel(double lossRate, double errorRate) {
+    private List<byte[]> lateQueue = new ArrayList<>();
+
+    public UnreliableChannel(double lossRate, double errorRate, double lateRate) {
         this.lossRate = lossRate;
         this.errorRate = errorRate;
+        this.lateRate = lateRate;
     }
 
     public boolean send(byte[] data){
         byte[] corruptdata = data.clone();
+        if (randomizer.nextDouble() < lateRate){
+            lateQueue.add(data);
+            return false;
+        }
         if (randomizer.nextDouble() < lossRate)
             return false;
+        if (randomizer.nextDouble() * lateQueue.size() > 1.0){
+            int getIndex = (int) (randomizer.nextDouble()*lateQueue.size());
+            byte[] getData = lateQueue.get(getIndex);
+            lateQueue.remove(getIndex);
+            this.setChanged();
+            this.notifyObservers(getData);
+        }
+
         for (int j=0; j < corruptdata.length; j++)
             if (randomizer.nextDouble() < errorRate)
                 corruptdata[j] += randomizer.nextDouble();
